4d80a1aa3e72a361f1fda8834b1e748bda98c311
Motjiang <motjiangbrilliant@gmail.com>
2025-01-24 20:01:44 +0200

    Added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,HomeComponent],
+  template: `
+  <main>
+    <header>
+      <div><h1>Homes</h1></div>
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
