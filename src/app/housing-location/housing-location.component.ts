78e076b055b391fb37d8d8b59fbf227b361b55c3
Motjiang <motjiangbrilliant@gmail.com>
2025-01-29 15:40:09 +0200

    Added the @input(), or data sharing between components

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
   `,
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
 
+  @Input() housingLocation!: HousingLocation;
 }
