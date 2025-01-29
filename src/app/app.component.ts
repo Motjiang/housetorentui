5aa579deca2f91d03a452dc1bc225f79cabc54c5
Motjiang <motjiangbrilliant@gmail.com>
2025-01-29 16:49:01 +0200

    Added a router Module to accommodate the details page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [
+    HomeComponent,
+    RouterLink,
+    RouterOutlet,
+  ],
   template: `
   <main>
+      <a [routerLink]="['/']">
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
     `,
